c75e954972b2fbfe0ec688dba048768f6e841a93
Rosa <ripy36@gmail.com>
2026-03-31 17:06:39 -0500

    Agregar enum EstadoReserva

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/enums/EstadoReserva.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/enums/EstadoReserva.java
@@ -1,4 +1,9 @@
 package com.orquicombeima.proyecto_orquideas.model.enums;
 
-public class EstadoReserva {
+public enum EstadoReserva {
+    ACTIVA,
+    EXPIRADA,
+    CONFIRMADA,
+    LIBERADA,
+    CANCELADA
 }
