9e9c6f8cf1cb72aea652c727c6a06cc9c373727d
Rosa <ripy36@gmail.com>
2026-03-31 21:50:24 -0500

    Agregar entidad Maceta

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/Maceta.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/Maceta.java
@@ -1,4 +1,26 @@
 package com.orquicombeima.proyecto_orquideas.model;
 
-public class Maceta {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "macetas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "id")
+
+
+public class Maceta extends Producto {
+
+    @Column(nullable = false)
+    private String material;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private String estilo;
 }
