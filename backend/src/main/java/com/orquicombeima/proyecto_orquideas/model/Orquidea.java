ed14271439eb68b71ae58d4af7b99883b176e6c5
Rosa <ripy36@gmail.com>
2026-03-31 21:49:25 -0500

    Agregar entidad Orquidea

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/Orquidea.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/Orquidea.java
@@ -1,4 +1,32 @@
 package com.orquicombeima.proyecto_orquideas.model;
 
-public class Orquidea {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orquideas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "id")
+
+
+public class Orquidea extends Producto {
+
+    @Column(nullable = false)
+    private String variedad;
+
+    @Column(name = "color_flor")
+    private String colorFlor;
+
+    @Column(nullable = false)
+    private String tamano;
+
+    @Column(name = "nivel_cuidado")
+    private String nivelCuidado;
+
+    @Column(name = "tiempo_floracion")
+    private String tiempoFloracion;
 }
