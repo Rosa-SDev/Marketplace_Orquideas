a722e9a154fd93f37769e2115aabe38fda61b628
JuanseGallego <juansebastiangallegovillamil@gmail.com>
2026-04-01 12:25:25 -0500

    Crear entidad DireccionEnvio donde se especifica todo para la creación de un pedido

---
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/model/DireccionEnvio.java
@@ -0,0 +1,36 @@
+package com.orquicombeima.proyecto_orquideas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DireccionEnvio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "direccionEnvio")
+    private Pedido pedido;
+
+    @Column(nullable = false)
+    private String nombreDestinatario;
+
+    @Column(nullable = false)
+    private String telefonoDestinatario;
+
+    @Column(nullable = false)
+    private String departamento;
+
+    @Column(nullable = false)
+    private String ciudad;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    private String codigoPostal;
+    private String instruccionesAdicionales;
+}
