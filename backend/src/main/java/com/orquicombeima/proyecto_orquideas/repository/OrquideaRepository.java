a1fe9c381f8c9b84db78b392cf870b701bbfaa39
Rosa <ripy36@gmail.com>
2026-04-01 22:12:45 -0500

    Agregar comentarios a OrquideRepository

--- backend/src/main/java/com/orquicombeima/proyecto_orquideas/repository/OrquideaRepository.java
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/repository/OrquideaRepository.java
@@ -4,7 +4,7 @@ import com.orquicombeima.proyecto_orquideas.model.Orquidea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// @Repository le dice a Spring que esta interfaz maneja acceso a la BD
+// Repositorio para acceder a los datos de orquídeas en la BD
 // JpaRepository<Orquidea, Long> nos da métodos predefinidos para hacer CRUD (findAll, findById, save, deleteById...) sin tener que escribir código SQL
 
 @Repository
