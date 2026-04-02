32c5cf4ddc6e53a5381482b8b116b8bc0843dfb7
JuanseGallego <juansebastiangallegovillamil@gmail.com>
2026-04-02 15:02:47 -0500

    Crear el controller para el contenido de la pagina con los endpoints para listar todo el contenido de la pagina y giltrarlos por tipo

---
+++ backend/src/main/java/com/orquicombeima/proyecto_orquideas/controller/ContenidoPaginaController.java
@@ -0,0 +1,32 @@
+package com.orquicombeima.proyecto_orquideas.controller;
+
+import com.orquicombeima.proyecto_orquideas.dto.ContenidoPaginaDTO;
+import com.orquicombeima.proyecto_orquideas.service.ContenidoPaginaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contenido-pagina")
+@RequiredArgsConstructor
+public class ContenidoPaginaController {
+
+    private final ContenidoPaginaService contenidoPaginaService;
+
+    // Endpoint para obtener todo el contenido de la pagina
+    @GetMapping
+    public ResponseEntity<List<ContenidoPaginaDTO>> obtenerTodoElContenido() {
+         return ResponseEntity.ok(contenidoPaginaService.obtenerTodoElContenido());
+    }
+
+    // Endpoint para obtener contenido de la pagina por tipo
+    @GetMapping("/{tipo}")
+    public ResponseEntity<List<ContenidoPaginaDTO>> obtenerContenidoPorTipo(@PathVariable String tipo) {
+        return ResponseEntity.ok(contenidoPaginaService.obtenerContenidoPorTipo(tipo));
+    }
+}
